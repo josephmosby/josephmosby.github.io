b4fc72a1ccd4b722fa0d31ce25abc2cf2fb5d357
josephmosby <josephmosby@gmail.com>
2013-11-09 20:26:17 -0500

    Adding working menu bar

--- javascripts/mainscript.js
+++ javascripts/mainscript.js
@@ -37,7 +37,7 @@ function animateIntro() {
 function setCookie(c_name, value, exdays) {
 	var exdate = new Date();
 	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="_exdate.toUTCString());
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
 	document.cookie=c_name + "=" + c_value;
 }
 
