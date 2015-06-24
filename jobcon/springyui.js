cadb1c74bb2700807e85356b9aa03221b19a10c7
Joseph Mosby <josephmosby@gmail.com>
2015-06-24 15:54:39 -0400

    roboto

--- jobcon/springyui.js
+++ jobcon/springyui.js
@@ -28,8 +28,8 @@ Copyright (c) 2010 Dennis Hotson
 jQuery.fn.springy = function(params) {
 	var graph = this.graph = params.graph || new Springy.Graph();
 	var nodeStartFont = 16;
-	var nodeFont = "16px Verdana, sans-serif";
-	var edgeFont = "8px Verdana, sans-serif";
+	var nodeFont = "16px Roboto Condensed, sans-serif";
+	var edgeFont = "12px Roboto, sans-serif";
 	var stiffness = params.stiffness || 400.0;
 	var repulsion = params.repulsion || 400.0;
 	var damping = params.damping || 0.5;
